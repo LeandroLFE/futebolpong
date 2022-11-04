518872cc3e2bde57d47636893afa02c21663c620
LeandroLFE <lfe.leandro@gmail.com>
2022-11-04 11:12:35 -0300

    create a blank display

--- pong.py
+++ pong.py
@@ -1,3 +1,16 @@
 import pygame
 
 pygame.init()
+
+window = pygame.display.set_mode([1280, 720])
+title = pygame.display.set_caption("LFE_Futebol_Pong")
+
+loop = True
+
+while loop:
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            loop = False    
+    
+    pygame.display.update()
